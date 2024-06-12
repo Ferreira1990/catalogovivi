b1766fe5cabd9a593acb51cf37cc5d0627f7988d
Cristian Ferreira <ferreira.8390mail.com>
2024-06-12 14:09:00 -0400

    titulo

--- src/components/Headerinfo/Headerx.jsx
+++ src/components/Headerinfo/Headerx.jsx
@@ -6,7 +6,8 @@ const Headerx = () => {
     <>
     <div className='Containerehaderinfo'>
         <div className='Namebrand'>
-            <h1 data-aos="fade-up" data-aos-duration="3000">VIVI ART</h1>
+            <h1 data-aos="fade-up" data-aos-duration="3000">Essenza</h1>
+            <h2 data-aos="fade-up" data-aos-duration="3000">by Vivi art</h2>
             <p data-aos="fade-left" data-aos-duration="3000">Productos 100% naturales.</p>
         </div>
 
