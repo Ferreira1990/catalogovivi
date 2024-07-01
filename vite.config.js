2477609e3e44d61408a9e34186a55461e73557af
Cristian Ferreira <ferreira.8390mail.com>
2024-07-01 10:51:31 -0400

    url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://ferreira1990.github.io/catalogovivi'
+  base:'https://www.viviartchile.cl'
 })
