55d5a902615adafb00ddd0d885edf8881a9046f0
Cristian Ferreira <ferreira.8390mail.com>
2024-09-01 16:28:58 -0400

    bg

--- src/components/Footer/Footerx.jsx
+++ src/components/Footer/Footerx.jsx
@@ -11,7 +11,7 @@ const Footerx = () => {
                 <h4>Contacto</h4>
                 <p>Dirección: Siete oriente 2249, Santiago, Chile</p>
                 <p>Teléfono: +569 8400 3044</p>
-                <p>Email: Essenza@gmail.com</p>
+                <p>Email: viviartchile@gmail.com</p>
                 <p>Horario de Atención: Lun - Sab, 9:00 - 18:00</p>
             </div>
             <div className="footer-section">
@@ -19,13 +19,7 @@ const Footerx = () => {
                 <a href="#"><i class="fa-brands fa-facebook"></i></a>
                 <a href="#"><i class="fa-brands fa-instagram"></i></a>
                 <a href="#"><i class="fa-brands fa-tiktok"></i></a>
-            </div>
-            <div className="footer-section">
-                <h4>Métodos de Pago</h4>
-                <i class="fa-brands fa-cc-visa"></i>
-                <i class="fa-brands fa-cc-mastercard"></i>
-                <i class="fa-regular fa-money-bill-1"></i>
-            </div>
+            </div>        
             <div className="footer-section">
                 <h4>Certificaciones</h4>
                 <i class="fa-solid fa-check"></i>Orgánico
